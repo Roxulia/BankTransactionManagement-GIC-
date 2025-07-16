da060ab1bb967eb09b13196e81711a66f89d234c
Roxulia <122478073+Roxulia@users.noreply.github.com>
2025-07-16 14:21:00 +0630

    add maintenence

--- src/Utility Functions/Maintenance.cbl
+++ src/Utility Functions/Maintenance.cbl
@@ -9,7 +9,23 @@
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT section.
        DATA DIVISION.
+<<<<<<< Updated upstream
        FILE SECTION.
+=======
+        FILE SECTION.
+       FD userfile.
+       01 userdata.
+           05 UID      PIC 9(5).
+           05 UName    PIC X(20).
+           05 ULoginName PIC X(25).
+           05 UEncodedPassword PIC X(32).
+           05 UAddress PIC X(20).
+           05 UPhone PIC x(9).
+           05 UBalance PIC 9(10)V99.
+           05 UTrxCount PIC 9(5).
+           05 UDate PIC 9(6).
+           05 UTime PIC 9(6).
+>>>>>>> Stashed changes
        WORKING-STORAGE SECTION.
        linkage SECTION.
        PROCEDURE DIVISION.
