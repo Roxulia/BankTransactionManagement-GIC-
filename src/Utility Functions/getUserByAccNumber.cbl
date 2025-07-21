83763bf7e302b27c60f4d1571f9704f7c4e713c2
Roxulia <122478073+Roxulia@users.noreply.github.com>
2025-07-21 13:03:02 +0630

    Added getUserByAccNumber and modify get functions
    Modified UserAccount related field values

--- src/Utility Functions/getUserByAccNumber.cbl
+++ src/Utility Functions/getUserByAccNumber.cbl
@@ -1,11 +1,11 @@
-******************************************************************
-      * Author: Sat Paing Thu
-      * Date: 10.7.2025
-      * Purpose: Pyaw Pya Woo
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
       * Tectonics: cobc
       ******************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. getUserByID.
+       PROGRAM-ID. getUserByAccNumber.
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
@@ -33,25 +33,13 @@
            05 UTime PIC 9(6).
 
        WORKING-STORAGE SECTION.
-       01 Cached-UID      PIC 9(5).
-       01 Cached-Data.
-           05 C-UID       PIC 9(5).
-           05 C-UName     PIC X(20).
-           05 C-ULogin    PIC X(25).
-           05 C-UPassword PIC X(32).
-           05 C-UAddress  PIC X(20).
-           05 C-UPhone    PIC x(9).
-           05 C-UBalance  PIC 9(10)V99.
-           05 C-TrxCount  pic 9(5).
-           05 C-UDate     PIC 9(8).
-           05 C-UTime     PIC 9(6).
        01 EOF-Flag PIC X(1) VALUE 'N'.
        01 ws-fs pic x(2).
        01  ret-found pic x.
-       01  ws-uid pic 9(5).
+       01  ws-uid pic 9(16).
 
        LINKAGE SECTION.
-       01 InputUID1 PIC 9(5).
+       01 InputUID1 PIC 9(16).
        01  statusCode pic xx.
        01 ReturnUserData.
            05 RET-UID      PIC 9(5).
@@ -73,22 +61,7 @@
             INITIALIZE ws-uid
             move InputUID1 to ws-uid
             MOVE 'N' TO RET-Found
-      *      IF ws-uid = Cached-UID and ws-uid not EQUAL zero
-               *>DISPLAY "NO NEED FILE READ"
-      *         MOVE C-UID       TO RET-UID
-      *         MOVE C-UName     TO RET-UName
-      *         MOVE C-ULogin    TO RET-ULoginName
-      *         MOVE C-UPassword TO RET-UEncodedPassword
-      *         MOVE C-UAddress  TO RET-UAddress
-      *         MOVE C-UPhone    TO RET-UPhone
-      *         MOVE C-UBalance  TO RET-UBalance
-      *         Move C-TrxCount  to RET-TrxCount
-      *         MOVE C-UDate     TO RET-UDate
-      *         MOVE C-UTime     TO RET-UTime
-      *         MOVE 'Y' TO RET-Found
-      *         move "00" to statusCode
-      *         go to back-to-main
-      *     END-IF
+      *
             OPEN INPUT testfile
             IF WS-FS NOT = "00"
                move "99" to statusCode
@@ -101,7 +74,7 @@
                         move "96" to statusCode
                     NOT AT END
                         *>DISPLAY userdata
-                        IF UID = ws-uid
+                        IF UAccNumber = ws-uid
                             *>DISPLAY userdata
                             MOVE UID         TO RET-UID
                             MOVE UName       TO RET-UName
@@ -120,17 +93,7 @@
                             MOVE UTime TO RET-UTime
                             MOVE 'Y' TO RET-Found
                             MOVE 'Y' TO EOF-Flag  *> Stop after finding
-                           MOVE ws-uid        TO Cached-UID
-                           MOVE RET-UID          TO C-UID
-                           MOVE RET-UName        TO C-UName
-                           MOVE RET-ULoginName   TO C-ULogin
-                           MOVE RET-UEncodedPassword TO C-UPassword
-                           MOVE RET-UAddress     TO C-UAddress
-                           MOVE RET-UPhone       TO C-UPhone
-                           MOVE RET-UBalance     TO C-UBalance
-                           move RET-TrxCount     to C-TrxCount
-                           MOVE RET-UDate        TO C-UDate
-                           MOVE RET-UTime        TO C-UTime
+
                            move "00" to statusCode
                         END-IF
                 END-READ
@@ -142,4 +105,4 @@
        back-to-main.
            exit PROGRAM.
 
-       END PROGRAM getUserByID.
+       END PROGRAM getUserByAccNumber.
